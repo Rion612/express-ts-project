031def61aeb0c4d3bc7e98f17d1a8aae4d5ec539
Md. Nazmul hasan <mdnazmul.hasan@bjitgroup.com>
2024-08-29 00:21:58 +0600

    Response text modified

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+  res.send('Hello, TypeScript with Express!!!');
 });
 
 app.listen(port, () => {
